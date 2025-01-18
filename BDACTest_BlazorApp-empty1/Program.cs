237534df6564b8f368946e217ce6f3e0bca5774f
Steve Townend <steve@townendmail.co.uk>
2025-01-18 19:36:58 +0000

    Testing basic menu layout

--- BDACTest_BlazorApp-empty1/Program.cs
+++ BDACTest_BlazorApp-empty1/Program.cs
@@ -1,11 +1,16 @@
 using BDACTest_BlazorApp_empty1;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+// 28.*.*
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzY2NDY5N0AzMjM4MmUzMDJlMzBlcTNkaVU0d1kyQXZMbzRHZnJOMGFBMngzSUs2TUU1UkU2LzYxcFZ5T2JJPQ==");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
